8796c055252e47fd511ba53099cf0104df92354c
Paul Rail <paul.a.rail@gmail.com>
2019-06-28 19:54:54 -0400

    begining project

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: 'orange',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15
+  }
+}
+
+export default Header;
