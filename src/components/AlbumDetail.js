5bac881a0c5a968b6a2c0cbfb46354d8582c0b14
Paul Rail <paul.a.rail@gmail.com>
2019-07-02 17:56:22 -0400

    important: placing two objects side-by-side

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -35,20 +35,65 @@
 
 
 // And even further/deeper:
+// import React from 'react';
+// import { View , Text } from 'react-native';
+// import Card from './Card';
+// import CardSection from './CardSection';
+
+// const AlbumDetail = (props) => {
+//   /* here we will pass any element between <Card></Card> to the Card component, they are available as {props.children} */
+//   return (
+//     <Card>
+//       <CardSection>
+//         <Text>{props.album.title}</Text>
+//       </CardSection>
+//     </Card>
+//   );
+// };
+
+// export default AlbumDetail;
+
+
+// lining up elements/components side-by-side in react native 
+// with separating in different View containers and flexDirection: 'row':
 import React from 'react';
-import { View , Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+
+// NOTE: to clean up the length of the props.album....
+// see video 4 from folder 8 at 3:00 min
+
 
 const AlbumDetail = (props) => {
   /* here we will pass any element between <Card></Card> to the Card component, they are available as {props.children} */
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image           
+            source={{ uri: props.album.thumbnail_image }} 
+            style={styles.albumThumbnailStyle}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
 };
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  albumThumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+}
 
 export default AlbumDetail;
