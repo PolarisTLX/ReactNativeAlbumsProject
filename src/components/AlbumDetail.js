7637423ab2bfa8c778b85c0ab1a9287b6cd64cc5
Paul Rail <paul.a.rail@gmail.com>
2019-07-02 12:28:22 -0400

    passing a component as props and its elements as {props.children}

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+// const AlbumDetail = (props) => {
+  //   return (
+    //     <View>                              <-------This is changed
+    //       <Text>{props.album.title}</Text>
+    //     </View>                             <-------This is changed
+    //   )
+    // };
+
+// To pass a component another component:
+// in this case we are passing this AlbumDetail to the Card component:
+import Card from './Card';
+
 const AlbumDetail = (props) => {
   return (
-    <View>
+    <Card>
+      {/* here we will pass any element between <Card></Card> to the Card component, they are available as {props.children} */}
       <Text>{props.album.title}</Text>
-    </View>
+    </Card>
   )
 };
 
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+  return (
+    <View style={styles.constainerStyle}>
+      {/* props.children is to render any components that get PASSED to this */}
+      {/* In Other Words: passed components show up as props.children */}
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  constainerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    // shadow likely does not work on android:
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
+};
+
+export default Card;
